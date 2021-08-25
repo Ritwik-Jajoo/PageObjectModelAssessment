281b0fc15e8b6241a1a5f0239c9d3e67d15f5f85
Ritwik Jajoo <ritwik.jajoo@getfareye.com>
2021-08-25 22:49:52 +0530

    adding setup and utils class

---
+++ src/main/java/Utilities/Utils.java
@@ -0,0 +1,59 @@
+package Utilities;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class Utils extends Setup {
+
+    public static void holdExecution(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static WebElement wait(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static void click(WebElement element) {
+        try {
+            wait(element);
+            element.click();
+        } catch (NoSuchElementException e) {
+            element.click();
+            holdExecution(10);
+        } catch (StaleElementReferenceException e) {
+            element.click();
+            holdExecution(10);
+        }
+    }
+
+    public static void enterText(WebElement element, String text) {
+        try {
+            wait(element).clear();
+            element.sendKeys(text);
+        } catch (NoSuchElementException e) {
+            element.sendKeys(text);
+            holdExecution(10);
+        } catch (StaleElementReferenceException e) {
+            element.sendKeys(text);
+            holdExecution(10);
+        }
+    }
+
+    public static boolean isDisplayed(WebElement element) {
+        boolean flag = false;
+        try {
+            if (element.isDisplayed())
+                flag = true;
+        } catch (NoSuchElementException e) {
+            flag = false;
+        }
+        return flag;
+    }
+}
